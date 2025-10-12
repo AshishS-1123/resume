14dd32d9512ad35fc3dd69bbf3d2c729f94d581f
Ashish Shevale <shevaleashish@gmail.com>
2025-10-12 16:06:45 +0530

    Update resume

--- generator/index.js
+++ generator/index.js
@@ -5,14 +5,27 @@ const sectionHeading = (heading) => ({
   marginTop: 8,
 });
 
-const sectionSubHeading = (first, second) => ({
-  marginTop: 6,
-  marginBottom: 4,
-  columns: [
-    { text: first, width: '*', fontSize: 12, bold: true },
-    { text: second, width: 'auto' },
-  ],
-});
+const sectionSubHeading = (first, second, url) => {
+  if ((url || '').length !== 0) {
+    return {
+    marginTop: 6,
+    marginBottom: 4,
+    columns: [
+      { text: first, width: '*', fontSize: 12, bold: true },
+      { text: second, width: 'auto', link: url, color: '#3c51b4' },
+    ],
+  };
+  }
+
+  return {
+    marginTop: 6,
+    marginBottom: 4,
+    columns: [
+      { text: first, width: '*', fontSize: 12, bold: true },
+      { text: second, width: 'auto' },
+    ],
+  };
+};
 
 const sectionSubSubHeading = (heading) => ({
   text: heading,
@@ -23,9 +36,30 @@ const sectionSubSubHeading = (heading) => ({
 
 const listItems = (listItems = []) => ({
   ul: listItems.map(item => ({
-    text: item,
+    text: splitBold(item),
   })),
 });
+
+const splitBold = (text) => {
+  const result = [];
+  const regex = /\*\*(.*?)\*\*|([^*]+)/g;
+  let match;
+
+  while ((match = regex.exec(text)) !== null) {
+    if (match[1] !== undefined) {
+      // Bold text
+      result.push({ text: match[1], bold: true });
+    } else if (match[2] !== undefined) {
+      // Plain text
+      const plainText = match[2];
+      if (plainText) {
+        result.push({ text: plainText, color: '#111' });
+      }
+    }
+  }
+
+  return result;
+};
 
 const header = {
   columns: [
@@ -33,17 +67,26 @@ const header = {
       width: '*',
       stack: [
         { text: 'Ashish Shevale', fontSize: 16 },
-        { text: 'Flutter Developer | MERN Stack Developer', fontSize: 11 },
+        { text: 'Full-Stack Developer', fontSize: 11 },
       ],
     },
     {
       width: 'auto',
       stack: [
         { text: 'shevaleashish@gmail.com', alignment: 'right' },
-        { text: 'linkedin.com/in/ashish-shevale', alignment: 'right' },
         { text: '+91-7447354182', alignment: 'right' },
+        {
+          alignment: 'right',
+          text: [
+            { text: 'LinkedIn', link: 'https://linkedin.com/in/ashish-shevale', color: '#3c51b4' },
+            { text: ' | ' },
+            { text: 'GitHub', link: 'https://github.com/AshishS-1123', color: '#3c51b4' },
+            { text: ' | ' },
+            { text: 'StackOverflow', link: 'https://stackoverflow.com/users/14405606/ashish-shevale', color: '#3c51b4' },
+          ]
+        },
       ],
-    }
+    },
   ],
 };
 
@@ -51,76 +94,63 @@ const skills = {
   stack: [
     sectionHeading('Skills'),
     {
-      text: 'Dart, Flutter, BLoC, Drift, SQLite, GoRouter, Protocol Buffers, React, PostgreSQL, Node.js, Express, Redux, TDD, Jest, Enzyme, RESTful APIs, GraphQL, Git',
+      text: 'Javascript, PostgreSQL, GraphQL, React, Express, REST APIs, AWS (S3, EC2, RDS), CI/CD (Jenkins, GitHub Actions), Unit Testing, Integration Testing, API Design, Agile',
     }
   ],
 };
 
 const workExperience = {
+  marginTop: 8,
   stack: [
     sectionHeading('Work Experience'),
-    sectionSubHeading('Fullstack Developer - Tata Consultancy Services', `August 2022 - Present (${Number((((new Date().getFullYear() - 2022) * 12 + (new Date().getMonth() - 8 + 1)) / 12).toFixed(1))} Years)`),
-    sectionSubSubHeading('Day to Day Responsibilities'),
+    sectionSubHeading('Tata Consultancy Services', `August 2022 - Present (${new Date().getFullYear() - 2022}+ Years)`),
     listItems([
-      'Worked on a project management SPA built using React, Node, PostgreSQL and GraphQL for users to track different aspects of their factories using KPIs, graphs, tables and reports.',
-      // 'Built complex workfows to enable users to take action against low KPIs and analyze trends & patterns. Generated automated Excel and PDF reports for business meetings.',
-      // 'Collaborated closely with business analysts, end-users and other stakeholders to deliver features and obtain feedback.',
-      'Conceptualized and delivered 15+ comprehensive dashboards, often developing both front-end and back-end.',
-      'Optimized the performance of SQL queries and developed multiple back-end stories.',
-      // 'Designed and maintained a suite of reusable React components.',
-      // 'Conducted in-depth exploration of npm libraries like pdfmake, victory & xlsx to develop POCs for critical features.',
-      'Mentored junior developers on the codebase and requirements, while conducting in-depth code reviews to enforce coding standards, validate database/API design, and catch logical issues.',
-    ]),
-    sectionSubSubHeading('Performance Improvements and Code Refactors'),
-    listItems([
-      'Migrated UI and API codebase and dependencies from Node 8 to 12, and later to Node 20. Also migrated the Jenkins pipeline and reduced deployment time from 30 minutes to 12 minutes.',
-      'Implemented route-based code splitting and optimized caching with a service worker (Workbox), reducing page load time by 84%.',
-      'Used SQL operations, AWS S3 and cron jobs to pre-generate reports to reduce download time from 6 minutes to 2 seconds.',
-      'Led the redesign efforts for a dashboard by creating components with unit tests and applying Test Driven',
-      'Development. Refactoring and code reuse reduced development time by 24 days.',
-    ]),
-    sectionSubSubHeading('Awards and Recognition'),
-    listItems([
-      'Recognized with the Star of the Quarter Award.',
-      'Received the highest rating in two appraisals.',
-      'Participated in weekly internal coding contests and won ₹1,000 worth of vouchers.',
-      'Cleared internal technical and soft-skills assessment in frst attempt.',
+      'Built **data-driven workflows** enabling clients to act on KPI drops and improve operational efficiency.',
+      'Partnered with analysts and stakeholders to deliver user-focused features through iterative feedback cycles.',
+      '**Mentored junior developers** and conducted in-depth** code reviews** to enforce coding standards, validate database/API design, and catch logical issues.',
+      '**Lowered page load time** to <3 seconds through route-based code splitting and advanced service-worker caching.',
+      '**Optimized SQL queries**, developed multiple back-end stories, and maintained a suite of **reusable React components**.',
+      'Migrated Jenkins CI/CD pipeline and **reduced deployment time** from 30 minutes to 12 minutes.',
+      'Used SQL operations, **AWS S3**, and cron jobs to pre-generate reports to **reduce download time** from 6 minutes to 2 seconds.',
+      'Led the redesign of a legacy dashboard by creating components with **Test-Driven Development (TDD)**. Refactoring and component reuse reduced development time by 24 days.',
+      'Migrated UI and API repository, dependencies, **webpack** config and **Jenkins** pipelines from Node 8 to Node 20.',
+      'Recognized with the **Star of the Quarter Award**.',
     ]),
   ],
 };
 
 const projects = {
+  marginTop: 8,
   stack: [
     sectionHeading('Projects'),
-    sectionSubHeading('Moderation - Financial Planner', 'Deployed to Android Play Store'),
+    sectionSubHeading('Moderation (Financial Planner App)', 'Android Play Store', 'https://play.google.com/store/apps/details?id=com.ashishshevale.moderation'),
     listItems([
-      'Built using Flutter, BLoC for state management and Drift & SQLite for storing user data.',
-      'Applied Clean Architecture principles to structure the codebase into data layer, abstract repository, repository implementation, service layer, and feature modules.',
-      'Used Protocol Buffers to serialize user events to create encrypted data backups.',
-      'Built a custom code generator (using source_gen and analyzer) to auto-generate randomized test data for event classes, for validating backup and serialization functionality.',
-      'Integrated with Google Cloud APIs (Drive and Sheets) to support encrypted data backup/restore and loading investment data.',
-      'Wrote integration tests and database migration tests for backwards compatibility.',
-      'Used AdMob to display rewarded ads and RevenueCat for premium subscriptions.',
-      'Features Implemented - Budget Planner, Shopping Lists, Data Backup, Habit Tracker, Analytics, Excel Data Import and Liquidity Alerts.'
+      'Owned the **full app lifecycle** - from UI/UX design and feature implementation to automated testing, CI/CD setup and Play Store deployment, ensuring a seamless release process.',
+      'Implemented **clean architecture** with modular layers for data, repositories, services, and view to ensure scalability and maintainability.',
+      'Built a **custom code generator** (using source_gen and analyzer) to auto-generate randomized data for automated tests.',
+      'Wrote **integration tests** and **database migration tests** for backward compatibility across app updates.',
+      'Integrated with Google Cloud APIs (Drive and Sheets) to support encrypted data backup and restore',
+      'Used **AdMob** (rewarded ads) and **RevenueCat** (monthly subscriptions) for monetization.',
+      'Features Implemented - Budget Planner, Shopping Lists, Data Backup, Habit Tracker, Analytics, Excel Data Import, and Liquidity Alerts.'
+    ]),
+
+    sectionSubHeading('Clickup Clone', 'GitHub (71 Stars, 21 Forks)', 'https://github.com/AshishS-1123/ClickUp-Clone'),
+    listItems([
+      'Built a clone of the ClickUp project management tool using **Next.js, Express and MongoDB**.',
+      'Designed **REST APIs** and hierarchical data model for workspaces, folders and tasks to support nested relationships and querying.',
+      'Used **Redux** & ** Thunk**, and **Container-Component** architecture to maintain separation of concerns between UI and data.',
+      'Developed Kanban board for visualizing tasks and drag-and-drop updates.'
     ]),
   ],
 };
 
-const profiles = {
-  stack: [
-    sectionHeading('Coding Profiles'),
-    sectionSubHeading('GitHub', 'github.com/AshishS-1123'),
-    { text: '54 repositories, 4 badges, 60 stars earned and 5 open-source contributions.', marginBottom: 4 },
-    sectionSubHeading('Stack Overfow', 'stackoverfow.com/users/14405606/ashish-shevale'),
-    { text: 'Answered 30+ questions, 10 badges and 370+ reputation.' },
-  ],
-};
 
 const education = {
+  marginTop: 8,
   stack: [
     sectionHeading('Education'),
-    sectionSubHeading('B.E. Electronics and Telecommunications', 'CGPA: 9.5'),
-    { text: 'Government College of Engineering and Research, Avasari' },
+    sectionSubHeading('B.E. Electronics and Telecommunications (July 2022)', 'CGPA: 9.5 / 10.0'),
+    { text: 'Government College of Engineering and Research' },
   ],
 };
 
@@ -138,13 +168,13 @@ const docDef = {
   info: {
     title: 'Ashish Shevale - Flutter Developer',
     author: 'Ashish Shevale',
-    subject: 'Dart,Flutter,SQL,Development,Integration Test,Firebase,Mobile,Widget,dependency injection,apis,libraries,android,ios,bloc,state,provider,getx,Cloud,Play store,App Store,FFI,UI/UX,debugging,bugs,crashes,JavaScript (ES6+), TypeScript, HTML5, CSS3, JQuery, Bootstrap, React.js (17+), React Router, React Query, Material UI, Next.js, Redux, React Toolkit, Test Driven Development, TDD/BDD, Unit test, Jasmine, Jest, REST, RESTful APIs, GraphQL, Mongo DB, PostgreSQL, Sqlite, NoSQL, SQL, MERN, PERN, MEAN, Webpack, Babel, Tailwind, SASS, SCSS Lazy Loading, Memoization, Component Optimization, GitHub Actions, Jenkins, CI/CD, AWS, Azure, Git, JWT, OAuth, SSO, SAML, Authentication, Authorization, Bug free, User Experience, Quality Standards, Documentation, Best practices, frontend development, backend development, architectural and design skills, frontend libraries, DOM manipulation, Reusability, Microservices, latest technologies, cross-functional teams, Agile,',
-    keywords: 'Dart,Flutter,SQL,Development,Integration Test,Firebase,Mobile,Widget,dependency injection,apis,libraries,android,ios,bloc,state,provider,getx,Cloud,Play store,App Store,FFI,UI/UX,debugging,bugs,crashes,JavaScript (ES6+), TypeScript, HTML5, CSS3, JQuery, Bootstrap, React.js (17+), React Router, React Query, Material UI, Next.js, Redux, React Toolkit, Test Driven Development, TDD/BDD, Unit test, Jasmine, Jest, REST, RESTful APIs, GraphQL, Mongo DB, PostgreSQL, Sqlite, NoSQL, SQL, MERN, PERN, MEAN, Webpack, Babel, Tailwind, SASS, SCSS Lazy Loading, Memoization, Component Optimization, GitHub Actions, Jenkins, CI/CD, AWS, Azure, Git, JWT, OAuth, SSO, SAML, Authentication, Authorization, Bug free, User Experience, Quality Standards, Documentation, Best practices, frontend development, backend development, architectural and design skills, frontend libraries, DOM manipulation, Reusability, Microservices, latest technologies, cross-functional teams, Agile,',
+    subject: 'Dart,Flutter,SQL,Development,Integration Test,Firebase,Mobile,Widget,dependency injection,apis,libraries,android,ios,bloc,state,provider,getx,Cloud,Play store,App Store,FFI,UI/UX,debugging,bugs,crashes,JavaScript (ES6+),TypeScript,HTML5,CSS3,JQuery,Bootstrap,React.js (17+),React Router,React Query,Material UI,Next.js,Redux,React Toolkit,Test Driven Development,TDD/BDD,Unit test,Jasmine,Jest,REST,RESTful APIs,GraphQL,Mongo DB,PostgreSQL,Sqlite,NoSQL,SQL,MERN,PERN,MEAN,Webpack,Babel,Tailwind,SASS,SCSS Lazy Loading,Memoization,Component Optimization,GitHub Actions,Jenkins,CI/CD,AWS,Azure,Git,JWT,OAuth,SSO,SAML,Authentication,Authorization,Bug free,User Experience,Quality Standards,Documentation,Best practices,frontend development,backend development,architectural and design skills,frontend libraries,DOM manipulation,Reusability,Microservices,latest technologies,cross-functional teams,Agile,AI,artificial inteligence,langchain,gpt,Gen AI,NLP,Spacy,tensorflow,pytorch,RAG,agent,copilot,MCP,LoRA',
+    keywords: 'Dart,Flutter,SQL,Development,Integration Test,Firebase,Mobile,Widget,dependency injection,apis,libraries,android,ios,bloc,state,provider,getx,Cloud,Play store,App Store,FFI,UI/UX,debugging,bugs,crashes,JavaScript (ES6+),TypeScript,HTML5,CSS3,JQuery,Bootstrap,React.js (17+),React Router,React Query,Material UI,Next.js,Redux,React Toolkit,Test Driven Development,TDD/BDD,Unit test,Jasmine,Jest,REST,RESTful APIs,GraphQL,Mongo DB,PostgreSQL,Sqlite,NoSQL,SQL,MERN,PERN,MEAN,Webpack,Babel,Tailwind,SASS,SCSS Lazy Loading,Memoization,Component Optimization,GitHub Actions,Jenkins,CI/CD,AWS,Azure,Git,JWT,OAuth,SSO,SAML,Authentication,Authorization,Bug free,User Experience,Quality Standards,Documentation,Best practices,frontend development,backend development,architectural and design skills,frontend libraries,DOM manipulation,Reusability,Microservices,latest technologies,cross-functional teams,Agile,AI,artificial inteligence,langchain,gpt,Gen AI,NLP,Spacy,tensorflow,pytorch,RAG,agent,copilot,MCP,LoRA',
   },
   pageSize: 'A4',
   pageMargins: [16, 16, 16, 16],
   defaultStyle: {
-    fontSize: 11,
+    fontSize: 12,
   },
   content: [
     header,
@@ -163,7 +193,7 @@ const docDef = {
     skills,
     workExperience,
     projects,
-    profiles,
+    // profiles,
     education,
   ],
 };
